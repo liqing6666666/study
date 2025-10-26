a408879e094b35fec8c1b9fca5d91bcdc19518e9
liqing6666666 <202321111078@mail.scuec.edu.cn>
2025-10-26 23:05:46 +0800

    添加了javascript函数学习博客

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -5,13 +5,14 @@ export const zhNavbar = navbar([
   {
     text: "博文",
     icon: "pen-to-square",
-    prefix: "/blogs/",
+    prefix: "/",
     children: [
-      { text: "解决github静态部署的图片不显示问题", icon: "pen-to-square", link: "2025_10_20_20/" },
-      { text: "markdown学习", icon: "pen-to-square", link: "2025_10_21_20/" },
-      { text: "解决github远程连接不上的问题", icon: "pen-to-square", link: "2025_10_21_22/" },
-      { text: "yaml学习", icon: "pen-to-square", link: "2025_10_22_9/" },
-      { text: "git学习", icon: "pen-to-square", link: "2025_10_22_14/" },
+      { text: "解决github静态部署的图片不显示问题", icon: "pen-to-square", link: "blogs/2025_10_20_20/" },
+      { text: "markdown学习", icon: "pen-to-square", link: "blogs/2025_10_21_20/" },
+      { text: "解决github远程连接不上的问题", icon: "pen-to-square", link: "blogs/2025_10_21_22/" },
+      { text: "yaml学习", icon: "pen-to-square", link: "blogs/2025_10_22_9/" },
+      { text: "git学习", icon: "pen-to-square", link: "blogs/2025_10_22_14/" },
+      { text: "Javascript学习", icon: "pen-to-square", link: "new_blogs/2025_10_26_13" },
     ],
   },
 ]);
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -3,7 +3,7 @@ import { sidebar } from "vuepress-theme-hope";
 export const zhSidebar = sidebar({
   "/": [
     {
-      text: "博客文章",
+      text: "博客1",
       icon: "book",
       prefix: "blogs/",
       children: [
@@ -12,7 +12,14 @@ export const zhSidebar = sidebar({
         "2025_10_21_22/",
         "2025_10_22_9/",
         "2025_10_22_14/",
+        
       ],
+    },
+    {
+      text: "博客2",
+      icon: "book",
+      prefix: "new_blogs/",
+      children: "structure",
     },
   ],
 });
